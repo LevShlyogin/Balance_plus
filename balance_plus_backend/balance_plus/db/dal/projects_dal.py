b5dbc423e72b56851173e1cbfb950047d8a006d2
lrshlyogin <ooooooorrrrr@gmail.com>
2025-06-23 17:52:19 +0500

    feat: добавлены функции для работы с проектами

--- balance_plus_backend/balance_plus/db/dal/projects_dal.py
+++ balance_plus_backend/balance_plus/db/dal/projects_dal.py
@@ -1,1 +1,22 @@
 # TODO: DAL для проектов 
+
+from typing import Optional, Sequence
+from uuid import UUID
+from sqlalchemy.ext.asyncio import AsyncSession
+from .. import models
+
+async def create_project(db: AsyncSession, name: str, gitlab_project_id: int, gitlab_repo_path: str, created_by_user_id: UUID) -> models.Project:
+    """Сохраняет метаданные нового проекта в БД."""
+    ...
+
+async def get_project_by_id(db: AsyncSession, project_id: UUID) -> Optional[models.Project]:
+    """Находит проект по его внутреннему UUID."""
+    ...
+
+async def get_project_by_gitlab_id(db: AsyncSession, gitlab_project_id: int) -> Optional[models.Project]:
+    """Находит проект по его ID в GitLab."""
+    ...
+
+async def list_all_projects(db: AsyncSession, skip: int = 0, limit: int = 100) -> Sequence[models.Project]:
+    """Возвращает список всех проектов с пагинацией."""
+    ... 
