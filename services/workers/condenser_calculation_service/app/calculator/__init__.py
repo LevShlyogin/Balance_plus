f4b5a97efb98c675492323378bc426f3ea092951
lrshlyogin <ooooooorrrrr@gmail.com>
2025-09-30 09:51:48 +0500

    feat: добавлены стратегии расчёта для сервиса конденсаторов, включая Berman, Metro Vickers, VKU и Table Pressure

---
+++ services/workers/condenser_calculation_service/app/calculator/__init__.py
@@ -0,0 +1,11 @@
+from .berman import BermanStrategy
+from .metro_vickers import MetroVickersStrategy
+from .vku import VKUStrategy
+from .table_pressure import TablePressureStrategy
+
+__all__ = [
+    'BermanStrategy',
+    'MetroVickersStrategy',
+    'VKUStrategy',
+    'TablePressureStrategy'
+]
